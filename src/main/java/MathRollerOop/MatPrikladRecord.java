afde81afe6a3d1fc82cb49e3c9a6c5c7b065a32f
Erdodi <mrerdodi@gmail.com>
2022-11-10 10:43:29 +0100

    second operation, third number

--- src/main/java/MathRollerOop/MatPrikladRecord.java
+++ src/main/java/MathRollerOop/MatPrikladRecord.java
@@ -1,27 +1,64 @@
 package MathRollerOop;
 
-public record MatPrikladRecord(int order, Operation operation, int firstNumber, int secondNumber) {
+public record MatPrikladRecord(
+    int order,
+    Operation operation,
+    Operation operationSec,
+    int firstNumber,
+    int secondNumber,
+    int thirdNumber) {
 
   public String prikladPreZiaka() {
-    String znamienko;
-    if (operation == Operation.PLUS) {
-      znamienko = "+";
+    String znamienko1;
+    String znamienko2;
+    if (operation == Operation.PLUS && operationSec == Operation.PLUS) {
+      znamienko1 = "+";
+      znamienko2 = "+";
     } else {
-      znamienko = "-";
+      if (operation == Operation.PLUS && operationSec == Operation.MINUS) {
+        znamienko1 = "+";
+        znamienko2 = "-";
+      } else {
+        if (operation == Operation.MINUS && operationSec == Operation.PLUS) {
+          znamienko1 = "-";
+          znamienko2 = "+";
+        } else {
+          znamienko1 = "-";
+          znamienko2 = "-";
+        }
+      }
     }
-    return "%s. %s %s %s = ?".formatted(order, firstNumber, znamienko, secondNumber);
+    return "%s. %s %s %s %s %s = ?"
+        .formatted(order, firstNumber, znamienko1, secondNumber, znamienko2, thirdNumber);
   }
 
   public String prikladPreUcitela() {
-    String znamienko;
+    String znamienko1;
+    String znamienko2;
     int result;
-    if (operation == Operation.PLUS) {
-      znamienko = "+";
-      result = firstNumber + secondNumber;
+
+    if (operation == Operation.PLUS && operationSec == Operation.PLUS) {
+      znamienko1 = "+";
+      znamienko2 = "+";
+      result = firstNumber + secondNumber + thirdNumber;
     } else {
-      znamienko = "-";
-      result = firstNumber - secondNumber;
+      if (operation == Operation.PLUS && operationSec == Operation.MINUS) {
+        znamienko1 = "+";
+        znamienko2 = "-";
+        result = firstNumber + secondNumber - thirdNumber;
+      } else {
+        if (operation == Operation.MINUS && operationSec == Operation.PLUS) {
+          znamienko1 = "-";
+          znamienko2 = "+";
+          result = firstNumber - secondNumber + thirdNumber;
+        } else {
+          znamienko1 = "-";
+          znamienko2 = "-";
+          result = firstNumber - secondNumber - thirdNumber;
+        }
+      }
     }
-    return "%s. %s %s %s = %s".formatted(order, firstNumber, znamienko, secondNumber, result);
+    return "%s. %s %s %s %s %s = %s"
+        .formatted(order, firstNumber, znamienko1, secondNumber, znamienko2, thirdNumber, result);
   }
 }
